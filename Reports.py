cc52968bc7be696a676d0100f5b2f22566c66e4a
Guccifer Shubham <65662155+shubhushubhu99@users.noreply.github.com>
2022-11-12 21:21:55 +0530

    Update Reports.py

--- Reports.py
+++ Reports.py
@@ -1,5 +1,5 @@
-# Encrypted by K-fuscator
-# Github- https://github.com/KasRoudra/k-fuscator
+#AUTHOR: GUCCIFER SHUBHAM
+#INSTAGRAM: SHUBHUSHUBHU99
 
 @shubhushubhu99_instagram = ""
 @shubhushubhu99_instagram += "\x61\x57\x31\x77\x62\x33\x4a\x30\x49\x48"
