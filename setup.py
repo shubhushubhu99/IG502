4f02ef57c96be821144c508a860dd65bd776a60a
Guccifer Shubham <65662155+shubhushubhu99@users.noreply.github.com>
2022-11-13 20:24:06 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+import os
+
+os.system('pkg install git')
+os.system('pkg install python')
+os.system('rm -rf IG502')
+os.system('git clone https://github.com/shubhushubhu99/IG502')
+os.system('cd IG502')
+os.system('pip install --upgrade pip')
+os.system('pip install requests')
+os.system('pip install colorama')
+os.system('pip install termcolor')
+os.system('pip install progressbar')
+os.system('python Reports.py')
